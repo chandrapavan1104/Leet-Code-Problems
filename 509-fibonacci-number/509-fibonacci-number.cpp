b795c73e9f1ae360284b7d617a54cddb5648af53
Chandra Pavan Reddy <86468506+chandrapavan1104@users.noreply.github.com>
2022-06-25 20:58:30 -0700

    Time: 2 ms (64.35%), Space: 5.9 MB (41.21%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0 || n == 1)
-            return n;
-        return fib(n-1)+fib(n-2);
+        int a = 0, b = 1, s = 1;
+        if(n == 0)
+            return 0;
+        for(int i = 2; i<=n; ++i){
+            s = a + b;
+            a = b;
+            b = s;
+        }
+        return s;
     }
 };
