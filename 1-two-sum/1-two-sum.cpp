0a5a7b622a78c34bac17f0bc61112247d4b11761
Chandra Pavan Reddy <86468506+chandrapavan1104@users.noreply.github.com>
2022-08-20 11:21:43 -0700

    Time: 22 ms (60.16%), Space: 12.1 MB (12.19%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,19 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
         unordered_map<int,int> temp;
-        for(int i=0;i<nums.size(); i++){
-            temp.insert({nums[i],i});
-        }
-        for(int i=0;i<nums.size(); i++){
-            int k = target - nums[i];
-            cout<<k;
-            if(temp.find(k) != temp.end()){
-                if(i != temp[k]){
+        for(int i=0; i<nums.size(); i++){
+            if(temp.find(target - nums[i]) != temp.end()){
+                ans.push_back(temp[target - nums[i]]);
                 ans.push_back(i);
-                ans.push_back(temp[k]);
-                break;
-                }
             }
+            else
+                temp.insert({nums[i], i});
         }
         return ans;
     }
