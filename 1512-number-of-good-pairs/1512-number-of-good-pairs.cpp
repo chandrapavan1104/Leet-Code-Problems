d17cabeec0c1b2e46dc42d0ed28f50ff2298a04a
Chandra Pavan Reddy <86468506+chandrapavan1104@users.noreply.github.com>
2022-08-20 10:37:10 -0700

    Time: 0 ms (100.00%), Space: 7.3 MB (15.23%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -4,17 +4,19 @@ public:
         int a[101] ={0}, s=0;
         unordered_map<int,int> count;
         for(int i=0; i<nums.size(); i++){
-            if(count.find(nums[i]) != count.end())
-                count[nums[i]]++;
+            if(count.find(nums[i]) != count.end()){
+                int t = count[nums[i]]/10000 + 1;
+                count[nums[i]] = t * 10000 + t*(t-1)/2;
+            }
             else
-                count.insert({nums[i], 1});
+                count.insert({nums[i], 10000*1});
         }
         
         for(auto x: count)
             cout<<x.first<<" "<<x.second<<endl;
         
         for(auto x: count){
-            s += x.second*(x.second-1)/2;
+            s += x.second % 10000;
         }
         return s;
     }
