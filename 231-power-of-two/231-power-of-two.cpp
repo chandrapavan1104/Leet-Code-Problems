0e5bca3c8c7784f56c1d1a60238c369f3b2e3935
Chandra Pavan Reddy <86468506+chandrapavan1104@users.noreply.github.com>
2022-08-21 09:01:00 -0700

    Time: 7 ms (15.33%), Space: 5.6 MB (99.08%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(long long int n) {
-        cout<<n<<" ";
-        if(n == 0)
-            return false;
-        if(n == 1)
-            return true;
-        if(n & 1 == 1)
-            return false;
-        return isPowerOfTwo(n >> 1);
+        return n>0 && !(n & n-1);
     }
 };
