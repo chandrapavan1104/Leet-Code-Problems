8c1c29060051dc15987ccca3bb6fed0742e3bd87
Chandra Pavan Reddy <86468506+chandrapavan1104@users.noreply.github.com>
2022-09-08 19:39:19 -0700

    Time: 4 ms (25.73%), Space: 5.9 MB (67.84%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,8 +4,18 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        if(!isBadVersion(n))
-            return n+1;
-        return firstBadVersion(n-1);
+        int first = 1;
+        int last = n;
+        int curr;
+        while (first <= last) {                
+            if (isBadVersion(first)) return first;
+            curr = first + (last-first) / 2;
+            if (isBadVersion(curr)) {
+                last = curr;
+            } else {
+                first = curr+1;
+            }
         }
+        return first;
+    }
 };
